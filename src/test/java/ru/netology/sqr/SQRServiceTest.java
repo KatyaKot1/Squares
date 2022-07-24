4edb9ee82b644f4849c94bfb773e616f7d0d04e5
Екатерина Бакланова <ekakotelevskaya@yandex.ru>
2022-07-24 12:32:39 +0300

    all files are added

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class SQRServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(files ="src/test/java/data.test")
+    public void testNumbersSquares(int expected, int min, int max) {
+        SQRService service = new SQRService();
+
+        int actual = service.NumberSquaresInRange(min, max);
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
